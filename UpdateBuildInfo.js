1e3c1a8adb8560b0ef5158b9b917882745cab54c
grinnius <gr.innie+github@gmail.com>
2024-10-29 23:58:34 +0200

    Log messages introduced
    Log messages

--- UpdateBuildInfo.js
+++ UpdateBuildInfo.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log(`(grinnius)---> Start UpdateBuildInfo.js`);
+
 // File path to store the build info
 const outputDir = path.join(__dirname, 'LeaveMyTextAlone');
 if (!fs.existsSync(outputDir)){
     fs.mkdirSync(outputDir);
 }
 const buildInfoPath = path.join(outputDir, 'buildInfo.json');
+
+console.log(`(grinnius)---> outputDir:${outputDir}`);
 
 // Load the existing build number from environment variables or start with 1
 const buildNumber = process.env.BUILD_NUMBER ? parseInt(process.env.BUILD_NUMBER, 10) + 1 : 1;
@@ -19,10 +23,10 @@ const buildInfo = {
 };
 
 fs.writeFileSync(buildInfoPath, JSON.stringify(buildInfo, null, 2), 'utf-8');
-console.log(`Build #${buildNumber} - ${timestamp}`);
+console.log(`(grinnius)---> Build #${buildNumber} - ${timestamp}`);
 
 // Update the environment variable for Netlify
 fs.writeFileSync(
-  path.join(__dirname, '.env'),
+  path.join(outputDir, '.env'),
   `BUILD_NUMBER=${buildNumber}\n`
 );
