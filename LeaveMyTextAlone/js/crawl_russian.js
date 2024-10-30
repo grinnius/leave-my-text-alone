bedc44a33e59d89305903af3b0cd565b8387afb2
AlexG <grinnie@gmail.com>
2024-10-30 23:01:11 +0200

    Russian crawl mapping externalized

---
+++ LeaveMyTextAlone/js/crawl_russian.js
@@ -0,0 +1,72 @@
+/**
+ * Set up russian capital letters no crawl
+ * @param arr
+ */
+function init_crawl_russian_capital(arr) {
+    // Russian capitals
+    arr['А'] = "AΑ";
+    arr['Б'] = "Б";
+    arr['В'] = "BΒ";
+    arr['Г'] = "Γ厂";
+    arr['Д'] = "Д";
+    arr['Е'] = "EΕ";
+    arr['Ж'] = "Ж";
+    arr['З'] = "3";
+    arr['И'] = "И";
+    arr['К'] = "KΚ";
+    arr['Л'] = "Λ人几入";
+    arr['М'] = "MΜᛖ";
+    arr['Н'] = "HΗዝ";
+    arr['О'] = "O0ΘΟዕ";
+    arr['П'] = "Π";
+    arr['Р'] = "PΡ";
+    arr['С'] = "C";
+    arr['Т'] = "TΤፕ";
+    arr['У'] = "YΥ";
+    arr['Ф'] = "中Φቅ";
+    arr['Х'] = "XΧ";
+    arr['Ц'] = "Ц";
+    arr['Ч'] = "Ч";
+    arr['Ш'] = "山";
+    arr['Щ'] = "Щ";
+    arr['Ъ'] = "Ъ";
+    arr['Ы'] = "Ы";
+    arr['Ь'] = "b";
+    arr['Э'] = "Э";
+    arr['Ю'] = "Ю";
+    arr['Я'] = "Я";
+}
+
+function init_crawl_russian_small(arr) {
+    arr['а'] = "aα⍺";
+    arr['б'] = "6ნ";
+    arr['в'] = "в";
+    arr['г'] = "г";
+    arr['д'] = "д";
+    arr['е'] = "eϱ";
+    arr['ж'] = "ж";
+    arr['з'] = "3";
+    arr['и'] = "ս";
+    arr['к'] = "kκ";
+    arr['л'] = "л";
+    arr['м'] = "ϻ";
+    arr['н'] = "н";
+    arr['о'] = "σഠ";
+    arr['п'] = "п";
+    arr['р'] = "pρ";
+    arr['с'] = "c";
+    arr['т'] = "τ";
+    arr['у'] = "yყ";
+    arr['ф'] = "φ";
+    arr['х'] = "xχ";
+    arr['ц'] = "ц";
+    arr['ч'] = "પ";
+    arr['ш'] = "ա";
+    arr['щ'] = "щ";
+    arr['ъ'] = "ъ";
+    arr['ы'] = "ы";
+    arr['ь'] = "ь";
+    arr['э'] = "э";
+    arr['ю'] = "ю";
+    arr['я'] = "я";
+}
