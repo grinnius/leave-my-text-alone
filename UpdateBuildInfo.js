b9e42a50be56a9200bef2e27ddfde9aa9e763af2
grinnius <gr.innie+github@gmail.com>
2024-10-30 08:15:38 +0200

    Some more logs

--- UpdateBuildInfo.js
+++ UpdateBuildInfo.js
@@ -10,7 +10,7 @@ if (!fs.existsSync(outputDir)){
 }
 const buildInfoPath = path.join(outputDir, 'buildInfo.json');
 
-console.log(`(grinnius)---> outputDir:${outputDir} buildInfoPath:%{buildInfoPath}`);
+console.log(`(grinnius)---> outputDir:${outputDir} buildInfoPath:${buildInfoPath}`);
 
 // Load the existing build number from environment variables or start with 1
 const buildNumber = process.env.BUILD_NUMBER ? parseInt(process.env.BUILD_NUMBER, 10) + 1 : 1;
@@ -19,7 +19,7 @@ const timestamp = new Date().toISOString();
 // Save the new build number and timestamp to a JSON file
 const buildInfo = {
   buildNumber,
-  timestamp,
+  timestamp
 };
 
 fs.writeFileSync(buildInfoPath, JSON.stringify(buildInfo, null, 2), 'utf-8');
@@ -30,3 +30,8 @@ fs.writeFileSync(
   path.join(outputDir, '.env'),
   `BUILD_NUMBER=${buildNumber}\n`
 );
+
+console.log(`(grinnius)---> Wrote to ${outputDir}.env Build #${buildNumber}`);
+
+
+
