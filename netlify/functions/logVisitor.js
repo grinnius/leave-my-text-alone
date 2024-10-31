858626deac9e80148ccf102fbfe3af4d2a74ed6c
AlexG <grinnie@gmail.com>
2024-10-31 13:34:00 +0200

    Leaving only a simple log message in the netlify function

--- netlify/functions/logVisitor.js
+++ netlify/functions/logVisitor.js
@@ -1,23 +1,9 @@
 // netlify/functions/logVisitor.js
-const fs = require('fs');
-const path = require('path');
 
 exports.handler = async (event) => {
     // Extract the visitor's IP address from headers
     const visitorIp = event.headers['x-nf-client-connection-ip'] || event.headers['client-ip'];
-    const timestamp = new Date().toISOString();
-
-    // Format the log entry
-    const logEntry = `Timestamp: ${timestamp}, IP: ${visitorIp} \n`;
-
-    // Define the path for the log file
-    const logFilePath = path.join('/tmp', 'visitor-log.txt');
-
-    // Append the log entry to the log file
-    fs.appendFileSync(logFilePath, logEntry);
-
-    const clientIp = event.headers['client-ip'];
-    console.log(`Client IP: ${clientIp}`);
+    console.log(`Client IP: ${visitorIp}`);
 
     return {
         statusCode: 200,
